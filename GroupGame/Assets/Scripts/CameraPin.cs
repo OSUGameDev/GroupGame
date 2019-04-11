14538eaa41fa6ddefa7449a4ece333c3c958dbe4
Moore <moorbren@oregonstate.edu>
2019-04-10 19:54:47 -0700

    First person camera/player movement. WIP

---
+++ GroupGame/Assets/Scripts/CameraPin.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPin : MonoBehaviour {
+
+	public Camera playerCamera;
+
+	// Use this for initialization
+	void Start () {
+		if (!playerCamera)
+			return;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.gameObject.transform.position = playerCamera.transform.position;
+	}
+}
---
+++ GroupGame/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float walkSpeed = 1000f;
+
+	Rigidbody playerBody;
+	Vector3 moveDirection;
+
+	void Awake(){
+		playerBody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float horizontalMovement = Input.GetAxisRaw ("Horizontal");
+		float verticalMovement = Input.GetAxisRaw ("Vertical");
+
+		moveDirection = (horizontalMovement * transform.right + verticalMovement * transform.forward).normalized;
+	}
+
+
+	//Physics updater
+	void FixedUpdate(){ 
+		Move ();
+	}
+
+	void Move(){
+		Vector3 yVelocity = new Vector3 (0, playerBody.velocity.y, 0);
+		//stores and then readds the yvelocity to allow the player to actually fall
+
+		playerBody.velocity = moveDirection * walkSpeed * Time.deltaTime;
+		playerBody.velocity += yVelocity;
+	}
+
+}
