dddb33366938ea9508799b7b86dc632323d30944
Michael Wang <wangshiyao96@gmail.com>
2019-04-15 15:21:43 -0700

    Updated the first person view

---
+++ GroupGame/Assets/Scripts/Weapon/LaserGun.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserGun : MonoBehaviour {
+
+    private LineRenderer laser;
+
+    private GameObject camera;
+
+	// Use this for initialization
+	void Start () {
+        laser = GetComponent<LineRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        //Check if the player hit the fire button
+
+	}
+
+    
+}
