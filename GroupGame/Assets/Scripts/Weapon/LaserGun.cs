c7b0d0cbf67a7c921dcce4afe5e2c21986fe97cb
Michael Wang <wangshiyao96@gmail.com>
2019-04-20 16:03:00 -0700

    Changed the laser script

--- GroupGame/Assets/Scripts/Weapon/LaserGun.cs
+++ GroupGame/Assets/Scripts/Weapon/LaserGun.cs
@@ -2,36 +2,43 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LaserGun : Gun {
+public class LaserGun : MonoBehaviour {
 
     private LineRenderer laser;
 
-    private float exist_time = 0.2f;        //How long for a singla fire exists
+    private float exist_time = 0.1f;        //How long for a singla fire exists
     private int exist_flag = 0;     //If the player fired
+
+    private Camera playerCam;
 
 
 
     // Use this for initialization
-    protected override void Start () {
-        base.Start();
-
+    void Start ()
+    {
         laser = GetComponent<LineRenderer>();       //Get the laser trace
+
+        playerCam = transform.parent.transform.GetChild(0).GetComponent<Camera>();
 	}
 
 
     //called in FixedUpdate
-    protected override void FireProjectile() {
+    void Update ()
+    {
 		//Check if the player hit the fire button
-		if (Input.GetButton ("Fire1") && exist_flag == 0) {
+		if (Input.GetButton ("Fire1") && exist_flag == 0)
+        {
 			//Cast a ray from center of the camera
 			Ray ray = playerCam.ViewportPointToRay (new Vector3 (0.5F, 0.5F, 0));
 			//Find the target point
 			RaycastHit hit;
 			//If the ray hit something
-			if (Physics.Raycast (ray, out hit)) {
+			if (Physics.Raycast (ray, out hit))
+            {
 				Debug.Log ("Fire! \n");
 				//If the target has a collider (hitable)
-				if (hit.collider != null) {           //If the target has a collider (hitable)
+				if (hit.collider != null)
+                {           //If the target has a collider (hitable)
 					Debug.Log ("Hit! \n");
 
 					//Shoot a laser to target
@@ -42,28 +49,29 @@ public class LaserGun : Gun {
 
 					//Check if hit the enemy
 					GameObject target = hit.collider.transform.gameObject;
-					if (target.tag == "Target Tester") {
+					if (target.tag == "Target Tester")
+                    {
 						Debug.Log ("Hit the target!");
 						target.GetComponent<EnemyHealth> ().TakeDamage (20);      //Make damage to target
 					}
-				} else {            //If the player shot something that 
-
 				}
 			}
-
-			if (exist_flag == 1) {
-				exist_time -= Time.deltaTime;       //The current exist timer of a single fire
-			}
-
-			//Check if the laser should be turned off
-			if (exist_time <= 0) {
-				laser.SetPosition (1, laser.GetPosition (0));     //Set the laser with 0 length
-
-				exist_flag = 0;
-
-				exist_time = 0.2f;      //Reset the fire timer
-			}
 		}
-	}
+
+        if (exist_flag == 1)
+        {
+            exist_time -= Time.deltaTime;       //The current exist timer of a single fire
+        }
+
+        //Check if the laser should be turned off
+        if (exist_time <= 0)
+        {
+            laser.SetPosition(1, laser.GetPosition(0));     //Set the laser with 0 length
+
+            exist_flag = 0;
+
+            exist_time = 0.2f;      //Reset the fire timer
+        }
+    }
     
 }
