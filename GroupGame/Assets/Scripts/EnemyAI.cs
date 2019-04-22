18d964e9ac583c632621dc0d33d7df2900d03148
z99o <z99o@users.noreply.github.com>
2019-04-21 23:48:16 -0700

    Enemy AI v0.1
    -EnemyAI prefab created
      *Enemy has spherical vision
      *Enemy will "engage with any object that has the "Player" tag by storing a reference to it, and rotating towards it.
    To Do:
    *Enemy needs to have physics and not be stationary.
    *Rotating by the y axis should be patched out since players can't do that
    *Basic movement routine should be created
    *Create placeholder entiries for implementation of the "ray gun" that another team member is making.

---
+++ GroupGame/Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+     // Start is called before the first frame update
+    public bool targetFound;
+    private GameObject currentTarget;
+    public GameObject aiVision;
+    public float lookSpeed = 3;
+  
+
+    void Start(){
+        aiVision = this.gameObject.transform.GetChild(0).gameObject;//get the Enemy Vision Object
+    }
+
+    // Update is called once per frame
+
+    void Update(){
+        lookForTarget();
+        if(targetFound){ //engages target as long as targetFound is true, it is important to make sure that the enemy does not try to engage with a null gameobject since that will cause a crash
+            engageTarget();
+        }
+    }
+
+    void lookForTarget(){ //Checks vision child for if it sees an enemy
+          targetFound = aiVision.GetComponent<EnemyAiVision>().opponentSpotted;
+        if(targetFound)
+            currentTarget = aiVision.GetComponent<EnemyAiVision>().enemySeen;
+    }
+    void engageTarget(){ //for not engaging the target will simply be rotating at it
+        Vector3 targetDir = currentTarget.transform.position - transform.position;
+
+        // The step size is equal to speed times frame time.
+        float step = lookSpeed * Time.deltaTime;
+
+        Vector3 newDir = Vector3.RotateTowards(transform.forward, targetDir, step, 0.0f);
+        Debug.DrawRay(transform.position, newDir, Color.red);
+
+        // Move our position a step closer to the target.
+        transform.rotation = Quaternion.LookRotation(newDir);
+    }
+}
---
+++ GroupGame/Assets/Scripts/EnemyAiVision.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAiVision : MonoBehaviour
+{
+    public bool opponentSpotted;
+    public GameObject enemySeen;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+   private void OnTriggerEnter(Collider other){
+     if (other.tag == "Player"){
+          opponentSpotted = true;
+          enemySeen = other.gameObject;
+     }
+    }
+   private void OnTriggerExit(Collider other)
+     {
+          if (other.tag == "Player"){
+               opponentSpotted = false;
+               enemySeen = null;
+          }
+     }
+}
