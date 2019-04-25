a8580ea47a7b447801e1be926b8a051194c6cd8a
Moore <moorbren@oregonstate.edu>
2019-04-24 19:29:14 -0700

    Fix laser gun merge

--- GroupGame/Assets/Scripts/Weapon/LaserGunExtend.cs
+++ GroupGame/Assets/Scripts/Weapon/LaserGunExtend.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LaserGun : Gun {
+public class LaserGunExtend : Gun {
 
     private LineRenderer laser;
 
