968a0da345386a36566bfcb8292c74a331ed85f1
Shinde <shindea@oregonstate.edu>
2019-05-02 15:37:21 -0700

    Added melee opertions without effect on enemy

---
+++ GroupGame/Assets/Scripts/LightAttack.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightAttack : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void onTriggerEnter(Collider other){
+		Debug.Log ("Are you Enemy?");
+		if (other.tag == "Enemy") {
+			Debug.Log ("Enemy Spotted");
+			other.transform.localScale += new Vector3 (0.5f, 0.5f, 0.5f);
+		}
+	}
+}
---
+++ GroupGame/Assets/Scripts/Melee.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Melee : MonoBehaviour {
+	public Collider lightAttack;
+	public Collider heavyAttack;
+	public bool isMeleeEquipped = true;
+	public float lightAttackDelay = 0.0f;
+	public float heavyAttackDelay = 0.5f;
+	public float lightAttackDuration = 0.5f;
+	public float heavyAttackDuration = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+		this.lightAttack.enabled = false;
+		this.heavyAttack.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (isMeleeEquipped) {
+			if (Input.GetMouseButtonDown(0)) {
+				StartCoroutine (HandleLightMelee ());
+			}
+
+			if (Input.GetMouseButtonDown (1)) {
+				StartCoroutine (HandleHeavyMelee ());
+			}
+		}
+	}
+
+	IEnumerator HandleHeavyMelee() {
+		Debug.Log ("Heavy Attack Started");
+		yield return new WaitForSeconds(heavyAttackDelay);
+		this.heavyAttack.enabled = true;
+		yield return new WaitForSeconds(heavyAttackDuration);
+		this.heavyAttack.enabled = false;
+		Debug.Log ("Heavy Attack Ended");
+	}
+
+	IEnumerator HandleLightMelee() {
+		Debug.Log ("Light Attack Started");
+		yield return new WaitForSeconds(lightAttackDelay);
+		this.lightAttack.enabled = true;
+		yield return new WaitForSeconds(lightAttackDuration);
+		this.lightAttack.enabled = false;
+		Debug.Log ("Light Attack Ended");
+	}
+}
