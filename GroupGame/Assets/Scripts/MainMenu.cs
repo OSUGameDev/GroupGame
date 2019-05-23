1dc0b86ec9631899be3d0d6e59df5a0ce1e0c743
Jason Stallkamp <jstallkamp@gmail.com>
2019-05-22 18:16:06 -0700

    Added basic main menu for single and multiplayer selection

---
+++ GroupGame/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine.Networking;
+
+public class MainMenu : MonoBehaviour
+{
+    public List<GameObject> Menus;
+    public GameObject NetworkManagerPrefab;
+    private GameObject InstantiatedNetworkManager;
+    public void ShowMenu(int index)
+    {
+        if (Menus.Count > index)
+        {
+            foreach (GameObject obj in Menus)
+            {
+                obj.SetActive(false);
+            }
+            Menus[index].SetActive(true);
+        }
+        else
+        {
+            Debug.LogError("Invalid Menu ID");
+        }
+    }
+
+
+    private string networkAddress = "localhost";
+    public void setNetworkAddress(string var)
+    {
+        this.networkAddress = var;
+    }
+    private int networkPort = 7777;
+    public void setNetworkPort(string var)
+    {
+        if (!int.TryParse(var, out this.networkPort))
+            this.networkPort = 7777;
+        
+    }
+    public void StartSinglePlayer()
+    {
+        SceneManager.LoadScene("BasicCharacter");
+    }
+    public void ConnectMultiplayer()
+    {
+        if (InstantiatedNetworkManager == null)
+            InstantiatedNetworkManager = Instantiate(NetworkManagerPrefab);
+        InstantiatedNetworkManager.GetComponent<NetworkManager>().onlineScene = "BasicCharacterNetworking";
+        InstantiatedNetworkManager.GetComponent<NetworkManager>().offlineScene = "MainMenu";
+        InstantiatedNetworkManager.GetComponent<NetworkManager>().networkAddress = networkAddress;
+        InstantiatedNetworkManager.GetComponent<NetworkManager>().networkPort = networkPort;
+        InstantiatedNetworkManager.GetComponent<NetworkManager>().StartClient();
+        
+    }
+    public void HostMultiPlayer()
+    {
+        if (InstantiatedNetworkManager == null)
+            InstantiatedNetworkManager = Instantiate(NetworkManagerPrefab);
+        InstantiatedNetworkManager.GetComponent<NetworkManager>().onlineScene = "BasicCharacterNetworking";
+        InstantiatedNetworkManager.GetComponent<NetworkManager>().offlineScene = "MainMenu";
+        InstantiatedNetworkManager.GetComponent<NetworkManager>().StartHost();
+    }
+
+    public void BeginQuit()
+    {
+        Application.Quit();
+        Debug.Log("quitting");
+    }
+}
